bfcb7adef7b837f3a95b449f7cc6a891973bf721
vitoroliveira <vitorofa@gmail.com>
2016-04-30 00:12:27 -0300

    nomes direcionais em pt-br adicionados

--- src/Sapo.java
+++ src/Sapo.java
@@ -25,18 +25,22 @@ public class Sapo {
         switch(tecla){
             case "up":
             case "w":
+            case "acima":
                 y -= largTela*(dt/vy);
                 break;
             case "left":
             case "a":
+            case "esquerda":
                 x -= largTela*(dt/vx);
                 break;
             case "right":
             case "d":
+            case "direita":
                 x += largTela*(dt/vx);
                 break;
             case "down":
             case "s":
+            case "abaixo":
                 y += largTela*(dt/vy);
                 break;
             default:
